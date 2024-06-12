2c138f2c30abbaa5c4c9c49473275bbb4076cb6d
James Nascimento <james.nascimento94@yahoo.com>
2024-06-12 16:33:19 +0000

    testing get 2 stocks function

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,104 @@
 'use strict';
+const StockModel = require('../models').Stock;
+const fetch = require('node-fetch');
+
+async function createStock(stock, like, ip) {
+  const newStock = new StockModel({
+    symbol: stock,
+    likes: like ? [ip] : [],
+  });
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+async function findStock(stock) {
+  return await StockModel.findOne({ symbol: stock }).exec();
+}
+
+async function saveStock(stock, like, ip) {
+  let saved = {};
+  const foundStock = await findStock(stock);
+  if (!foundStock) {
+    const createSaved = await createStock(stock, like, ip);
+    saved = createSaved;
+    return saved;
+  } else {
+    if (like && foundStock.likes.indexOf(ip) === -1) {
+      foundStock.likes.push(ip);
+    }
+    saved = await foundStock.save();
+    return saved;
+  }
+}
+
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
 
 module.exports = function (app) {
   // https://stock-price-checker-proxy.freecodecamp.rocks/
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async (req, res) => {
+        const { stock, like } = req.query;
+        if (Array.isArray(stock)) {
+          console.log("stocks", stock);
+
+          const { symbol, latestPrice } = await getStock(stock[0]);
+          const { symbol: symbol2, latestPrice: latestPrice2 } = await getStock(stock[1]);
+          const firstStock = await saveStock(stock[0], like, req.ip);
+          const secondStock = await saveStock(stock[1], like, req.ip);
+          
+          let stockData = [];
+          if (!symbol) {
+            stockData.push({
+              rel_likes: firstStock.likes.length - secondStock.likes.length
+            });
+          } else {
+            stockData.push({
+              symbol: symbol,
+              price: latestPrice,
+              rel_likes: firstStock.likes.length - secondStock.likes.length
+            });
+          }
+
+          if (!symbol2) {
+            stockData.push({
+              rel_likes: secondStock.likes.length - firstStock.likes.length
+            });
+          } else {
+            stockData.push({
+              symbol: symbol2,
+              price: latestPrice2,
+              rel_likes: secondStock.likes.length - firstStock.likes.length
+            });
+          }
+
+          res.json({
+            stockData,
+          });
+          return;
+        }
+        const { symbol, latestPrice } = await getStock(stock);
+        if (!symbol) {
+          res.json({ stockData: { likes: like ? 1 : 0}});
+          return;
+        }
+
+        const oneStockData = await saveStock(symbol, like, req.ip);
+        console.log("One Stock Data", oneStockData);
+
+        res.json({
+          stockData: {
+            stock: symbol,
+            price: latestPrice,
+            likes: oneStockData.likes.length
+          }
+        })
     });
     
 };
